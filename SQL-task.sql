b51307d69d74188daac08528f8768f9f5ecf6acd
Shaivik Thakar <shaivik008@gmail.com>
2024-02-03 14:29:52 +0000

    Created another table to apply prescription filter

--- SQL-task.sql
+++ SQL-task.sql
@@ -50,3 +50,60 @@ WITH CTE AS (
 DELETE FROM CTE WHERE RowNum > 1;
 
 SELECT * FROM #PatientObs;
+
+-- Getting patients that were prescribed the medicines related to shortness of breath (Formoterol Fumarate, Salmeterol Xinafoate, Vilanterol, Indacaterol, Olodaterol)
+-- This separate table was made as the patients that were prescribed the medicines resided in different postcodes apart from the top 2 postcodes with highest patient counts
+
+IF OBJECT_ID('tempdb..#PatientMeds') IS NOT NULL
+    DROP TABLE #PatientMeds;    
+SELECT 
+    dbo.patient.registration_guid,
+    patient_id,
+    CONCAT(patient_givenname, ' ', patient_surname) AS full_name,
+    emis_original_term, 
+    postcode, 
+    DATEDIFF(YEAR, dbo.patient.date_of_birth, GETDATE()) AS age, 
+    gender
+INTO #PatientMeds
+FROM dbo.medication
+FULL OUTER JOIN dbo.patient ON dbo.medication.registration_guid = dbo.patient.registration_guid
+WHERE emis_original_term LIKE '%Formoterol%'
+OR emis_original_term LIKE '%Fumarate%' 
+OR emis_original_term LIKE '%Xinafoate%'
+OR emis_original_term LIKE '%Salmeterol%'
+OR emis_original_term LIKE '%Vilanterol%'
+OR emis_original_term LIKE '%Indacaterol%'
+OR emis_original_term LIKE '%Olodaterol%'
+
+-- Exluding somkers
+AND dbo.medication.snomed_concept_id NOT IN (
+        SELECT snomed_concept_id
+        FROM dbo.clinical_codes
+        WHERE refset_simple_id = '999004211000230104'
+    )
+
+-- Excluding patients that weigh less than 40kg 
+AND snomed_concept_id NOT IN (27113001)
+
+-- Excluding patients that have COPD diagnosis
+AND dbo.medication.snomed_concept_id NOT IN (
+        SELECT snomed_concept_id
+        FROM dbo.clinical_codes
+        WHERE refset_simple_id = '999011571000230107'
+    )
+    
+-- Getting patients that have not opted out of taking part in the research
+AND dbo.medication.opt_out_9nu0_flag = 'FALSE';
+
+-- Used common table expression to remove duplicates from the PatientMeds table
+WITH CTE AS (
+    SELECT 
+        full_name,
+        patient_id,
+        emis_original_term,
+        ROW_NUMBER() OVER (PARTITION BY patient_id ORDER BY emis_original_term) AS RowNum
+    FROM #PatientMeds
+)
+DELETE FROM CTE WHERE RowNum > 1;
+
+SELECT* FROM #PatientMeds;
